4dc981c409a74eace8d0b23e36e65eafd6e95c9a
brian taylor vann <brian.t.vann@gmail.com>
2023-01-29 19:54:34 -0500

    remove old, add tsconfig

--- v0.1/deno/timestep.ts
+++
@@ -1,7 +0,0 @@
-export type {
-  TimestepContextInterface,
-  TimestepInterface,
-  TimestepStatus,
-} from "./fixed_timestep/fixed_timestep_types.ts";
-
-export { FixedTimestep as Timestep } from "./fixed_timestep/fixed_timestep.ts";
