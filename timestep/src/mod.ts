15b8ad6ae2295b6eeb32d1c077f522c76aaea986
Taylor Vann <brian.t.vann@gmail.com>
2024-07-04 13:36:50 -0700

    remove extra module

--- timestep/src/mod.ts
+++ timestep/src/mod.ts
@@ -1,57 +1,59 @@
-interface AccumulatorImpl {
-  timestamp: DOMHighResTimeStamp;
-  accumulator: number;
-}
-
-interface IntegrationCallback {
-  (accumlator: number): void;
+interface RendererImpl {
+	integrate: (stepMs: number, intervalMs: number) => void;
+	render: (lastStepMs: number, intervalMs: number) => void;
 }
 
 interface TimestepImpl {
-  start(): void;
-  stop(): void;
+	start(): void;
+	stop(): void;
 }
 
-class Accumulator implements AccumulatorImpl {
-  timestamp = performance.now();
-  accumulator = 0;
-}
+let min_step_6 = (1 / 6) * 1000;
 
 class Timestep implements TimestepImpl {
-  #ac: AccumulatorImpl;
-  #ic: IntegrationCallback;
-  #receipt?: number = undefined;
+	#timestamp = performance.now();
+	#time = 0;
+	#accumulator = 0;
+	#receipt?: number = undefined;
 
-  #boundLoop: (now: DOMHighResTimeStamp) => void;
+	#rc: RendererImpl;
+	#intervalMs: number;
+	#boundLoop: (now: DOMHighResTimeStamp) => void;
 
-  constructor(
-    callback: IntegrationCallback,
-  ) {
-    this.#ac = new Accumulator();
-    this.#ic = callback;
-    this.#boundLoop = this.#loop.bind(this);
-  }
+	constructor(
+		renderer: RendererImpl,
+		intervalMs: number,
+	) {
+		this.#rc = renderer;
+		this.#intervalMs = Math.max(min_step_6, intervalMs);
+		this.#boundLoop = this.#loop.bind(this);
+	}
 
-  start() {
-    if (this.#receipt) return;
-    this.#receipt = window.requestAnimationFrame(this.#boundLoop);
-  }
+	start() {
+		if (this.#receipt) return;
+		this.#receipt = window.requestAnimationFrame(this.#boundLoop);
+	}
 
-  stop() {
-    window.cancelAnimationFrame(this.#receipt);
-    this.#receipt = undefined;
-  }
+	stop() {
+		window.cancelAnimationFrame(this.#receipt);
+		this.#receipt = undefined;
+	}
 
-  #loop(now: DOMHighResTimeStamp) {
-    this.#ac.accumulator += now - this.#ac.timestamp;
+	#loop(now: DOMHighResTimeStamp) {
+		this.#accumulator += now - this.#timestamp;
+		this.#timestamp = now;
 
-    // loop through steps in external integrate function
-    // this way can skip or stop at a max step
-    this.#ic(this.#ac.accumulator);
-    this.#ac.timestamp = now;
-    this.#receipt = window.requestAnimationFrame(this.#boundLoop);
-  }
+		while (this.#accumulator > this.#intervalMs) {
+			this.#rc.integrate(this.#time, this.#intervalMs);
+
+			this.#accumulator -= this.#intervalMs;
+			this.#time += this.#intervalMs;
+		}
+
+		this.#rc.render(this.#accumulator, this.#intervalMs);
+		this.#receipt = window.requestAnimationFrame(this.#boundLoop);
+	}
 }
 
-export type { TimestepImpl, IntegrationCallback };
+export type { TimestepImpl, RendererImpl };
 export { Timestep };
